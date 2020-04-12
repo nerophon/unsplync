34c0c1c5496ce33ff14b933ea2078fd5bd6df011
Nicholas Bellerophon <commits@nerophon.com>
2020-04-12 21:46:08 +0100

    outline

--- unsplync.go
+++ unsplync.go
@@ -2,4 +2,15 @@ package main
 
 func main() {
 
+	// read configuration
+
+	// get the collection's photo metadata
+	// https://unsplash.com/documentation#get-a-collections-photos
+
+	// compare to local files, building a list of photos that need to be downloaded
+
+	// loop over photos that need to be downloaded
+	//   download the photo
+
+	// output results to stdout
 }
